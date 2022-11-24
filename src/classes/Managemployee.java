3c93f6eb14e5f5ca1642ce4d7ebf8847a5036d14
delokoseni <artur.b3lsh@gmail.com>
2022-11-24 21:36:55 +0700

    1

--- src/classes/Managemployee.java
+++ src/classes/Managemployee.java
@@ -3,33 +3,60 @@ package classes;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Managemployee extends Subemployee{
     private Subordinates subs;
+    private ArrayList<Integer> subList = new ArrayList<>(10);
     public Managemployee(){
         super();
         subs = new Subordinates();
+        for(int i = 0; i < 10; i++){
+            subList.add(0);
+        }
     }
 
     public Managemployee(int allfields) {
         super(allfields);
         subs = new Subordinates(allfields);
+        if(allfields >= 1)
+            for(int i = 0; i < allfields; i++)
+                subList.add(allfields);
+        else{
+            System.out.println("Указано недопустимое значение allfields для subList.");
+            for(int i = 0; i < 10; i++)
+                subList.add(0);
+        }
     }
 
-    public Managemployee(Experience exp, Hours hour, Jobtitle jt, int managerid, Subordinates subs){
+    public Managemployee(Experience exp, Hours hour, Jobtitle jt, int managerid,
+                         Subordinates subs, ArrayList<Integer> subList){
         super(exp, hour, jt, managerid);
         this.subs = subs;
+        this.subList = subList;
     }
 
     public void output() {
         super.output();
         subs.output();
+        System.out.println("Массив ID подчиненных:");
+        for(int i = 0; i < subList.size();i++){
+            System.out.println(subList.get(i));
+        }
     }
 
     /**метод ввода**/
     public void input(){
         super.input();
         subs.input();
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Введите количество подчиненных: ");
+        int n = inp.nextInt();
+        for(int i = 0; i < n;i++){
+            System.out.println("Введите ID подчиненного: ");
+            n = inp.nextInt();
+            subList.add(n);
+        }
     }
 
     /** Метод подсчета зарплаты **/
