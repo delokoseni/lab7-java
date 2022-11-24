76526b7c07be9cfecfeea901ddacdcf279ade9c1
delokoseni <artur.b3lsh@gmail.com>
2022-11-24 18:46:07 +0700

    Продемонстрирован в конструкторе производного класса с параметрами вызов конструктора базового класса

--- src/classes/Managemployee.java
+++ src/classes/Managemployee.java
@@ -6,5 +6,18 @@ import java.util.Scanner;
 
 public class Managemployee extends Subemployee{
     private Subordinates subs;
+    public Managemployee(){
+        super();
+        subs = new Subordinates();
+    }
 
+    public Managemployee(int allfields) {
+        super(allfields);
+        subs = new Subordinates(allfields);
+    }
+
+    public Managemployee(Experience exp, Hours hour, Jobtitle jt, int managerid, Subordinates subs){
+        super(exp, hour, jt, managerid);
+        this.subs = subs;
+    }
 }
--- src/classes/Subemployee.java
+++ src/classes/Subemployee.java
@@ -3,11 +3,33 @@ package classes;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
-import classes.Employee;
 
 public class Subemployee extends Employee {
     /**ID управляющего**/
     private int managerid;
+
+    public Subemployee(){
+        super();
+        managerid = 0;
+    }
+
+    public Subemployee(int allfields) {
+        super(allfields);
+        if(allfields >= 0)
+            managerid = allfields;
+        else
+            managerid = 0;
+    }
+
+    public Subemployee(Experience exp, Hours hour, Jobtitle jt, int managerid){
+        super(exp, hour, jt);
+        if(managerid >=0)
+            this.managerid = managerid;
+        else{
+            System.out.println("Указано недопустимое значение managerid.");
+            this.managerid = 0;
+        }
+    }
 
     /**Определение абстрактного метода**/
     @Override
