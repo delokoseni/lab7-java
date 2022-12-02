e2a905b56d6c28c05619c6cc4595623631f75760
delokoseni <artur.b3lsh@gmail.com>
2022-12-02 14:32:32 +0700

    Добавлен Vector в класс Managemployee

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,20 @@ import classes.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Salary sal = new Salary(10);
+        Experience exp = new Experience(8);
+        Hours hour = new Hours(140);
+        Jobtitle jt = new Jobtitle(3);
+        int managerid = 18;
+        Subordinates subs = new Subordinates(2);
+        Vector subList = new Vector(10);
+        for(int i = 0; i < 10; i++){
+            subList.add(subs);
+        }
         Subemployee AA = new Subemployee(50);
-        Managemployee BB = new Managemployee(50);
-        System.out.println("Зарплата Subemployee: " + AA.getsalary(sal));
-        System.out.println("Зарплата Managemployee: " + BB.getsalary(sal));
-        System.out.println("\n" + AA.toString() + "\n");
-        System.out.println("\n" + BB.toString() + "\n");
+        Managemployee BB = new Managemployee( exp, hour, jt, managerid, subs, subList);
     }
 }
--- src/classes/Managemployee.java
+++ src/classes/Managemployee.java
@@ -3,34 +3,37 @@ package classes;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.Vector;
 
 public class Managemployee extends Subemployee{
     private Subordinates subs;
-    private ArrayList<Integer> subList = new ArrayList<>(10);
+    private Vector subList;
     public Managemployee(){
         super();
         subs = new Subordinates();
+
+        subList = new Vector(10);
         for(int i = 0; i < 10; i++){
-            subList.add(0);
+            subList.add(subs);
         }
     }
 
     public Managemployee(int allfields) {
         super(allfields);
         subs = new Subordinates(allfields);
+        subList = new Vector(subs.getamount());
         if(allfields >= 1)
             for(int i = 0; i < allfields; i++)
                 subList.add(allfields);
         else{
             System.out.println("Указано недопустимое значение allfields для subList.");
-            for(int i = 0; i < 10; i++)
+            for(int i = 0; i < subs.getamount(); i++)
                 subList.add(0);
         }
     }
 
     public Managemployee(Experience exp, Hours hour, Jobtitle jt, int managerid,
-                         Subordinates subs, ArrayList<Integer> subList){
+                         Subordinates subs, Vector subList){
         super(exp, hour, jt, managerid);
         this.subs = subs;
         this.subList = subList;
@@ -40,13 +43,13 @@ public class Managemployee extends Subemployee{
         super.output();
         subs.output();
         System.out.println("Массив ID подчиненных:");
-        for(int i = 0; i < subList.size();i++){
+        for(int i = 0; i < subList.size(); i++){
             System.out.println(subList.get(i));
         }
     }
 
     /**метод ввода**/
-    public void input(){
+    /**public void input(){
         super.input();
         subs.input();
         Scanner inp = new Scanner(System.in);
@@ -55,12 +58,12 @@ public class Managemployee extends Subemployee{
         for(int i = 0; i < n;i++){
             System.out.println("Введите ID подчиненного: ");
             n = inp.nextInt();
-            subList.add(n);
+            //subList.add(n);
         }
     }
-
+**/
     /** Метод подсчета зарплаты **/
-    public int getsalary(Salary sal){
+   /** public int getsalary(Salary sal){
         int salary = super.getsalary(sal);
         salary += sal.allmoney(salary, subs);
         return salary;
@@ -70,4 +73,5 @@ public class Managemployee extends Subemployee{
         return super.toString() + "\n" +
                 subs.toString();
     }
+    **/
 }
